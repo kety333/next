3b7957e74b69d06926f5aa3aca514ad3e786aec6
kety <ketyd333@gmail.com>
2021-11-07 18:24:08 +0200

    next  movie - fix line btn

--- src/app/movies/movies.module.ts
+++ src/app/movies/movies.module.ts
@@ -5,13 +5,14 @@ import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import {MoviesRoutingModule} from "./movies-routing.module";
 import {SharedModule} from "../shared/shared.module";
 import { MovieRatingComponent } from './movie-details/movie-rating/movie-rating.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { HttpClientModule} from "@angular/common/http";
 import { MovieMoreDialogComponent } from './movie-details/movie-more-dialog/movie-more-dialog.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import { MovieInfoComponent } from './movie-details/movie-more-dialog/movie-info/movie-info.component';
 import { FilterPipe } from './pipe/filter.pipe';
 import {FormsModule} from "@angular/forms";
 import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatButtonModule} from "@angular/material/button";
 
 
 
@@ -31,7 +32,8 @@ import {MatTooltipModule} from "@angular/material/tooltip";
         HttpClientModule,
         MatDialogModule,
         FormsModule,
-        MatTooltipModule
+        MatTooltipModule,
+        MatButtonModule
     ],
   providers: []
 })
